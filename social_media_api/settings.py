6c5af42e15af590ad36b87b43aafbf4805580a15
Abokor Daoud <abokordaoud22@gmail.com>
2024-12-15 00:17:01 +0300

    Add JWT authentication configuration and update requirements

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -125,8 +125,9 @@ STATIC_URL = 'static/'
 # Rest Framework settings
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',

3d335e916d4018a70133ccbd6df542df4cdb690b
Abokor Daoud <abokordaoud22@gmail.com>
2024-12-20 16:36:50 +0300

    Use pre-built Pillow wheel and simplify build process

--- build.sh
+++ build.sh
@@ -2,10 +2,6 @@
 # exit on error
 set -o errexit
 
-# Install system dependencies for Pillow
-apt-get update
-apt-get install -y python3-dev python3-pip python3-setuptools libjpeg-dev zlib1g-dev
-
 pip install --upgrade pip
 pip install -r requirements.txt
 

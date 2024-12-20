2ad0438bbea218231df85f569acc2aa8bf473151
Abokor Daoud <abokordaoud22@gmail.com>
2024-12-20 16:58:10 +0300

    Switch to Python 3.11 and simplify dependencies

--- build.sh
+++ build.sh
@@ -2,15 +2,8 @@
 # exit on error
 set -o errexit
 
-# Install build dependencies
-pip install --upgrade pip setuptools wheel
-
-# Install packages that need compilation separately
-pip install --no-binary :all: psycopg2-binary==2.9.9
-pip install --no-binary :all: Pillow==10.1.0
-
-# Install remaining requirements
-grep -v "psycopg2-binary\|Pillow" requirements.txt | pip install -r /dev/stdin
+pip install --upgrade pip
+pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate --noinput

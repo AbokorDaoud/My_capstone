a868dc3b231283f429db870b3b1c852f000a7ff0
Abokor Daoud <abokordaoud22@gmail.com>
2024-12-20 16:51:17 +0300

    Revert to Python 3.13 and fix Pillow installation

--- build.sh
+++ build.sh
@@ -2,7 +2,14 @@
 # exit on error
 set -o errexit
 
+# Upgrade pip and install wheel first
 pip install --upgrade pip
+pip install wheel
+
+# Install Pillow first
+pip install --no-cache-dir Pillow==10.1.0
+
+# Install remaining requirements
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
